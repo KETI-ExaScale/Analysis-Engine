9ad2f9d86ab82261e65085a8cd73b4d3d60b3a4c
juhyunkil <wngus0331@gmail.com>
2023-11-14 17:00:49 +0900

    main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,28 @@
 package main
 
-import "analysis-engine/pkg/analysis"
+import (
+	"analysis-engine/pkg/analysis"
+	"context"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+)
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	var wg sync.WaitGroup
+
 	w := analysis.InitEngine()
-	w.Work()
+	wg.Add(2)
+	w.Work(ctx, &wg)
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	<-signalChan
+	cancel()
+	wg.Wait()
+	os.Exit(0)
 }
